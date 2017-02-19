f6544fcde7d27bc65f37520ae86f67f8be5ceb49
Ossey <xiaoyuan1314@me.com>
2017-02-19 19:19:04 +0800

    clear

---
+++ BackgroundDownloadDemo/BackgroundDownloadDemo/BackgroundDownload/AppDelegate+XYBackgroundTask.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate+XYBackgroundTask.h
+//  BackgroundDownloadDemo
+//
+//  Created by mofeini on 17/2/19.
+//  Copyright © 2017年 com.test.demo. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import "XYBackgroundDownloadProtocol.h"
+
+extern NSString *const XYDownloadProgressNotification;
+
+@interface AppDelegate (XYBackgroundTask) <XYBackgroundDownloadProtocol>
+
+@property (nonatomic, weak) id<XYBackgroundDownloadProtocol> backgroundDownloadDelegate;
+
+@end
